a29b722e8dd89566169cb062aa52c407d14c2f98
Redeem-Grimm-Satoshi <45304978+Redeem-Grimm-Satoshi@users.noreply.github.com>
2019-12-15 10:32:43 +0100

    Add files via upload

---
+++ src/AnagramFinder.jl
@@ -0,0 +1,27 @@
+#######################################################
+#-->Author: Redeem_Grimm
+#-->Purpose: Find Anagram Of A Word From A List Of Words
+########################################################
+
+using HTTP
+
+function is_anagram(firstWord,secondWord)
+    firstWordSorted=join(sort(collect(firstWord)))
+    secondWordSorted=join(sort(collect(secondWord)))
+    return firstWordSorted==secondWordSorted
+
+end
+
+function download_words(urlOfFile)
+   contentOfFile=HTTP.download(urlOfFile)
+   listOfWords=[lowercase(words) for words in readlines(contentOfFile)]
+   return listOfWords
+    
+end
+
+
+function find_anagram(word,words)
+
+   return filter(x-> is_anagram(x,word),words)
+     
+end
